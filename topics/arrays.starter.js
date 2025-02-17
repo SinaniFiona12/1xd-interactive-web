d448fdae31d73a42c58d484d0bce4715ee5d3abb
SinaniFiona12 <r1049215@student.thomasmore.be>
2025-02-17 18:28:35 +0100

    array challenge

--- topics/arrays.starter.js
+++ topics/arrays.starter.js
@@ -22,23 +22,33 @@ const drinks = ["icetea", "water", "chocomelk"];
 console.log(meals.pop());
 
 // 4 - loop over the meals array and print each meal to the console
-//??
+for (const element of meals) {
+  console.log(element);
+}
+
 // 5 - add a meal called `cereal` to the `meals` array, this time using the push() function, log the variable to the console
 meals.push("cereal");
 console.log(meals);
 
 // 6 - create a variable `breakfast` and find the position of `cereal` in the `meals` array
 // hint: use the indexOf() function - log the result to the console
-let breakfast 
+console.log(meals.indexOf("cereal"));
+
 
 /* CHALLENGE :: TIPJAR */
 // 7 - create an array `tipjar` that will contain all our tips, add the following tips: 1, 5, 2, 2, 1
-
+const tipjar = [1, 5, 2, 2, 1];
 // 8 - create a function `tip` that will add a tip to the `tipjar` array
 // hint: the function should take a parameter `amount` and add it to the `tipjar` array via `push()`
+const tip = tipjar.push(2);
 
 // 9 - call the function with a tip amount of 2 and log the `tipjar` array to the console
-
+console.log(tipjar)
 // 10 - create a function `total` that will calculate the total amount of the `tipjar` array
 // hint: use a loop to iterate over the array and add the values together, the function should return the result
 // log the total value of the tips to the console
+const total = tipjar.reduce((tip, currentTotal) => {
+  return tip + currentTotal
+}, 0)
+
+console.log(total);
